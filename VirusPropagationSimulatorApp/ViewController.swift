f729cd56df221e5064cc3022ab1648e4bba09c17
ILIA <dubenskiiilia@gmail.com>
2023-05-04 14:21:29 +0300

    Initial Commit

---
+++ VirusPropagationSimulatorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VirusPropagationSimulatorApp
+//
+//  Created by Илья Дубенский on 04.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
